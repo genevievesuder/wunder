19617ac84fefca5701e544921fbc0eac8293f543
Genevieve Suder <genevievesuder@gmail.com>
2023-08-15 06:23:25 -0700

    current update

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
 
 
 const Gallery = () => {
+const [isRed, setIsred] = useState(false)
+
+const changeColor = () => {
+  setIsred(currentState => !currentState)
+}
+
+
   return (
-    <div className="gallery">Gallery</div>
+    <div className="gallery">
+      { isRed ? (
+      <h1 onClick={changeColor}>Click me!</h1>
+      ) : (
+      <h1 className='red' onClick={changeColor}>Click me!</h1>
+      )}
+    </div>
   )
 }
 
